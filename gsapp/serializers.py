a11b01dc8c260292d4d22cc59ac9221dac7beb28
MordovinNik <mordovin_nikolya@mail.ru>
2022-07-18 22:59:23 +0300

    Added details in reports getters

--- gsapp/serializers.py
+++ gsapp/serializers.py
@@ -44,3 +44,23 @@ class ConcernPermissionsSerializer(serializers.ModelSerializer):
         model = ConcernPermissions
         fields = '__all__'
 
+
+class UserMiniSerializer(serializers.ModelSerializer):
+    """Вывод фамилии, имени и почты пользователя"""
+    class Meta:
+        model = Users
+        fields = ('id', 'first_name', 'last_name', 'email')
+
+
+class ReportsDetailSerializer(serializers.ModelSerializer):
+    """Отчеты с подробной информацией"""
+    report_type = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    dept = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    submitter = UserMiniSerializer()
+    assigned_user = UserMiniSerializer()
+    added_users = UserMiniSerializer(many=True)
+    subdept = serializers.SlugRelatedField(slug_field='name', read_only=True)
+
+    class Meta:
+        model = Reports
+        fields = '__all__'
--- gsapp/views.py
+++ gsapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_swagger.views import get_swagger_view
 from .models import *
 from rest_framework import viewsets
@@ -10,6 +12,16 @@ schema_view = get_swagger_view(title='Pastebin API')
 class ReportsAPIViewSet(viewsets.ModelViewSet):
     queryset = Reports.objects.all()
     serializer_class = ReportsSerializer
+
+    def list(self, request, *args, **kwargs):
+        reports = Reports.objects.all()
+        serializer = ReportsDetailSerializer(reports, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        reports = Reports.objects.all()
+        serializer = ReportsDetailSerializer(reports, many=True)
+        return Response(serializer.data)
 
 
 class SubDeptsAPIViewSet(viewsets.ModelViewSet):
